3604b58362fe505c56c652343de22f8b9e6b36bf
Ваня Хортоломей <hortolomei1989@gmail.com>
2023-05-02 11:14:25 +0300

    fix cors

--- src/services/EmailEvents.js
+++ src/services/EmailEvents.js
@@ -6,6 +6,7 @@ const emails = axios.create({
     headers: {
         "Accept": "application/json",
         "Content-Type": "application/json",
+        'Access-Control-Allow-Origin' : '*',
     },
 });
 
